e5e14a6cacc31bc1ec2288a530786e427582da41
taisia <tasyamozhanova@gmail.com>
2019-10-19 19:44:18 +0300

    after review

--- transport_telegram.py
+++ transport_telegram.py
@@ -1,11 +1,27 @@
 import requests
+import time
 
 
-# method for delivering a message in telegram
 def transport_telegram(message, webhook):
-    try:
-        requests.post(webhook, data={"message": message})  # sending the message by webhook
-        return True
-    except ValueError:
-        print('Wrong webhook')
-        return False
+    """
+    Method for delivering a message in telegram
+    message - it is a message you want to send in telegram
+    webhook - webhook for telegram
+    """
+    flag = 0
+    while flag < 10:  # if connecting error reconnects up to 10 times every second
+        try:
+            requests.post(webhook, data={"message": message}, timeout=(0.00001, 10))  # sending the message by webhook
+            return True
+        except ValueError:
+            print('Wrong webhook')
+            return False
+        except ConnectionError:
+            print("Connection Error")
+            flag += 1
+            time.sleep(1)
+        except requests.exceptions.ConnectTimeout:
+            print("Timeout error")
+            flag += 1
+            time.sleep(1)
+    return False
