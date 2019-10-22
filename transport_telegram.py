ecbef19019c4115efbdc262694c0d34835637249
taisia <tasyamozhanova@gmail.com>
2019-10-22 18:33:55 +0300

    with logging and retry

--- transport_telegram.py
+++ transport_telegram.py
@@ -3,8 +3,13 @@ from retrying import retry
 import logging
 
 
+def retry_if_connection_error(exception):
+    """If Connection error in requests return True, False otherwise"""
+    return isinstance(exception, requests.exceptions.ConnectionError)
+
+
 # retry if Connecting or timeout error for 3 time
-@retry((ConnectionError, requests.exceptions.ConnectTimeout), stop_max_attempt_number=3)
+@retry(retry_on_exception=retry_if_connection_error, stop_max_attempt_number=3)
 def transport_telegram(message, webhook):
     """
     Method for delivering a message in telegram
@@ -12,13 +17,12 @@ def transport_telegram(message, webhook):
     webhook - webhook for telegram
     """
     try:
-        requests.post(webhook, data={"message": message}, timeout=(0.00001, 10))  # sending the message by webhook
+        requests.post(webhook, data={"message": message}, timeout=10)  # sending the message by webhook
         return True
     except ValueError:
         logging.error('Wrong webhook')
         return False
-    except ConnectionError:
+    except requests.exceptions.ConnectionError:
         logging.error("Connection Error")
-    except requests.exceptions.ConnectTimeout:
-        logging.error("Timeout error")
     return False
+
