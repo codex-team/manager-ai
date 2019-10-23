4b99758ec867c7af9106c55e052b4e37712b1945
Ivan Vasilev <vanokvas@gmail.com>
2019-10-23 21:03:19 +0300

    Changed test section

--- tests/scenarios_tests/xpathscenario_test.py
+++ tests/scenarios_tests/xpathscenario_test.py
@@ -1,31 +1,54 @@
+import unittest
+import httpretty
 from scenarios.xpathscenario import XPathScenario
 
+SAMPLE_BODY = r"""
+<html>
+<head>
+  <title>This is a title</title>
+</head>
+<body>
+  <h1>Header 1</h1>
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+</body>
+</html>"""
+
 params_list = [
-    # {
-    #     'url': 'https://t.me/s/codex_team',
-    #     'xpath': '/html/body/main/div/section/div[20]/div[1]/div[2]/div[3]/div/span[3]/a/time',
-    #     'proxies': {
-    #     }
-    # },
     {
-        'url': 'https://github.com/codex-team/manager-ai/pulls',
-        'xpath': '//*[@id="issue_6"]/div/div[2]'
+        "url": "https://test_xpath.com",
+        "xpath": "/html/body/h1"
     },
     {
-        'url': 'https://www.forbes.ru',
-        'xpath': '//*[@id="top"]/div/div[1]/article'
+        "url": "https://test_xpath.com",
+        "xpath": "/html/body/p[1]"
     },
     {
-        'url': 'https://yandex.ru',
-        'xpath': '//*[@id="wd-_traffic"]/div/div[1]'
+        "url": "https://test_xpath.com",
+        "xpath": "/html/body/p[2]"
+    },
+    {
+        "url": "https://test_xpath.com",
+        "xpath": "/html/head/title"
     }
 ]
 
-print("Testing XPathScenario.run()...\n")
 
-for params in params_list:
-    my_scenario = XPathScenario(params)
-    print(my_scenario.get_html_element_by_xpath())
-    my_scenario.run()
+class TestXPathScenario(unittest.TestCase):
+    def setUp(self) -> None:
+        httpretty.enable()
+        httpretty.register_uri(httpretty.GET, "https://test_xpath.com",
+                               body=SAMPLE_BODY)
 
-print("\nCompleted. Checkout {XPathScenario.DEFAULT_TIMESTAMPS_FILE}")
+    def test_xpath_scenario(self):
+        for params in params_list:
+            scenario = XPathScenario(params)
+            scenario.run()
+
+    def tearDown(self) -> None:
+        httpretty.disable()
+        httpretty.reset()
+
+
+if __name__ == "__main__":
+    unittest.main()
