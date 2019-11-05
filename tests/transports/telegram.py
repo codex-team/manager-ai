218ed10770899da464dcb347551ea963f64a60bd
taisia <tasyamozhanova@gmail.com>
2019-11-05 16:21:24 +0300

    +tests

---
+++ tests/transports/telegram.py
@@ -0,0 +1,26 @@
+import unittest
+import httpretty
+from src.transports.telegram import transport_telegram
+
+
+class TestTransportTelegram(unittest.TestCase):
+    """
+        Class for testing transport_telegram
+    """
+
+    def setUp(self):  # test preparation method
+        def request_callback(request, uri, response_headers):  # what answer is right
+            ok = request.parsed_body["message"][0] == "Test telegram transports"
+            self.assertEqual(ok, True)
+            return [200, response_headers, "OK" if ok else "ERROR"]
+
+        httpretty.enable()
+        httpretty.register_uri(httpretty.POST, "http://transport_test.com/", body=request_callback)  # register fake http with request_callback
+
+    def tearDown(self):  # final test method
+        httpretty.reset()
+        httpretty.disable()
+
+    def test_first(self):
+        response = transport_telegram("Test telegram transports", "http://transport_test.com/")
+        self.assertEqual(response, True)
